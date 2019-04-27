6043ed776ccee43ce113c78f289e02a02422e423
Dario <molinadario46@yahoo.com>
2019-04-27 08:24:07 -0500

    cambio

--- ParqueaderoSwin/src/ec/ups/edu/parqueadero/dao/TicketDAO.java
+++ ParqueaderoSwin/src/ec/ups/edu/parqueadero/dao/TicketDAO.java
@@ -56,80 +56,6 @@ public class TicketDAO {
             return 0;
         }
     }
- 
-   public String[] selectTicketComprobante(int numero) throws SQLException{
-        Conectar con = new Conectar();
-        Connection conn = con.getConnection();
-        
-        String sql = "SELECT t.numero, t.fh_entrada, NOW(), MIN(TIMEDIFF(NOW(),t.fh_entrada)), ROUND(((TIMESTAMPDIFF(MINUTE,t.fh_entrada,NOW())))*0.01,2), ROUND((((TIMESTAMPDIFF(MINUTE,t.fh_entrada,NOW())))*0.01)*0.12,2), ROUND(((TIMESTAMPDIFF(MINUTE,t.fh_entrada,NOW()))*0.01)+(((TIMESTAMPDIFF(MINUTE,t.fh_entrada,NOW()))*0.01)*0.12),2),t.placa  FROM ticket t WHERE t.numero="+numero+";"; 
-        ResultSet rs = null;
-        PreparedStatement ps = null;
-        String[] array = new String[8];
-        
-        try {
-            ps = con.getConnection().prepareStatement(sql);
-            rs = ps.executeQuery();
-            
-            while(rs.next()){
-                array[0]= rs.getInt(1)+"";
-                array[1]= rs.getString(2);
-                array[2]= rs.getString(3);
-                array[3]= rs.getString(4);
-                array[4]= rs.getString(5);
-                array[5]= rs.getString(6);
-                array[6]= rs.getString(7);
-                array[7]= rs.getString(8);
-            } 
-        } catch (SQLException e) {
-            System.out.println("ERROR EN DAO  \n"+e);
-            return null;
-        }
-      return array;      
-    }
-   
-   public String[] selectTicketComprobantePagado(int numero) throws SQLException{
-        Conectar con = new Conectar();
-        Connection conn = con.getConnection();
-        
-        String sql = "SELECT t.numero, t.placa, DATE(c.fecha), t.fh_entrada, c.fh_salida, c.tiempo, c.total FROM ticketpagado t, comprobante c WHERE t.numero = c.numero and t.numero ="+numero+";"; 
-        ResultSet rs = null;
-        PreparedStatement ps = null;
-        String[] array = new String[8];
-        
-        try {
-            ps = con.getConnection().prepareStatement(sql);
-            rs = ps.executeQuery();
-            
-            while(rs.next()){
-                array[0]= rs.getInt(1)+"";
-                array[1]= rs.getString(2);
-                array[2]= rs.getString(3);
-                array[3]= rs.getString(4);
-                array[4]= rs.getString(5);
-                array[5]= rs.getString(6);              
-                array[6]= rs.getDouble(7)+"";
-            } 
-        } catch (SQLException e) {
-            System.out.println("ERROR EN DAO  \n"+e);
-            return null;
-        }
-      return array;      
-    }
-
-   public boolean DeleteTicketComprobante(String numero) throws SQLException{
-        Conectar con = new Conectar();
-        Connection conn = con.getConnection();
-        try {
-            CallableStatement deleteTicket = conn.prepareCall("{call deleteTicket(?)}");
-            deleteTicket.setString(1, numero);
-            deleteTicket.executeQuery();
-            System.out.println("TicketDAO Eliminado ");
-            return true; 
-        } catch (SQLException e) {
-            System.out.println("Error al Eliminar TicketDAO \nTIPO ERROR:  "+e);
-            return false;
-        }
-    }
    
    public ArrayList<Ticket> listTicket(){
         ArrayList<Ticket> listaTicket = new ArrayList<Ticket>();
@@ -162,38 +88,4 @@ public class TicketDAO {
         }
         return listaTicket;
     }
-   
-    public ArrayList<Ticketpagado> listTicketPagados(){
-        ArrayList<Ticketpagado> listaTicketsPagados = new ArrayList<Ticketpagado>();
-        Conectar conec = new Conectar();
-        String sql = "SELECT * FROM ticketpagado;";
-        ResultSet rs = null;
-        PreparedStatement ps = null;
-        try{
-            ps = conec.getConnection().prepareStatement(sql);
-            rs = ps.executeQuery();
-            while(rs.next()){
-                Ticketpagado ticp = new Ticketpagado();
-                ticp.setNumero(rs.getInt(1));
-                ticp.setFh_entrada(rs.getString(2));
-                ticp.setPlaca(rs.getString(3));
-                ticp.setFh_pago(rs.getString(4));
-                listaTicketsPagados.add(ticp);
-            }
-        }catch(SQLException ex){
-            System.out.println(ex.getMessage());
-            return null;
-        }catch(Exception ex){
-            System.out.println(ex.getMessage());
-            return null;
-        }finally{
-            try{
-                ps.close();
-                rs.close();
-                conec.desconectar();
-            }catch(Exception ex){return null;}
-        }
-        return listaTicketsPagados;
-    } 
-    
 }
--- ParqueaderoSwin/src/ec/ups/edu/parqueadero/on/PagoON.java
+++ ParqueaderoSwin/src/ec/ups/edu/parqueadero/on/PagoON.java
@@ -23,38 +23,16 @@ public class PagoON {
     private Ticket tic = new Ticket();
     
     public void RegistrarPago(String Codigo) throws SQLException{
-            veh.setPlaca(Codigo);
-            if(tdao.InsertTicket(veh)==true){
-                JOptionPane.showMessageDialog(null, "TICKET GENERADO");
-            }else{
-                JOptionPane.showMessageDialog(null, "ERROR DE GENERACION DE TICKET");
-            }
+           
+          
     }
 
-    public int SiguienteIdTicket(){
-        int siguienteNumero = 0;
-        if(tdao.selectSiguienteNumero()!=0){
-            return siguienteNumero = tdao.selectSiguienteNumero();
-        }else{
-            return siguienteNumero;
-        }        
-    }
+    public Double  CalcularMonto(){
      
-    public ArrayList<Ticket> ListarTickets(){
-        tdao = new TicketDAO();
-        Ticket tic= new Ticket();
-        ArrayList<Ticket> listaTickets = tdao.listTicket();
-        if(listaTickets.size() > 0){
-            for(int i=0; i< listaTickets.size(); i++){
-                String fila[] = new String[3];
-                tic = listaTickets.get(i);
-                fila[0] = tic.getCodigo()+"";
-                fila[1] = tic.getHoraEntrada()+"";
-                fila[2] = tic.getEspacioAsignado();
-            }
-        }
-        return listaTickets;
-    } 
-    
+        
+        
+        
+        return null;        
+    }
   
 }
